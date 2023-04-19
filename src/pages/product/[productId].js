2cbb4cf68ea657f8791187f163bc09f95ad5f635
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-04-19 17:58:25 +0600

    product id catch using use router

--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -1,8 +1,10 @@
-const ProductDetails
- = () => {
+import { useRouter } from "next/router";
+const ProductDetails = () => {
+    const router = useRouter();
+    const productId = router.query.productId;
     return (
         <div>
-            <h2>Product Details page</h2> 
+            <h2>Product Details page productId is :{productId} </h2> 
         </div>
     );
 };
